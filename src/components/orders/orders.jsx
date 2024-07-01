547ffb08b58da5cc0c1e410cd1d1aebcf219a7e8
Sahil Kalkal <sahilkalkal108@gmail.com>
2024-07-02 03:44:32 +0530

    captcha

---
+++ src/components/orders/orders.jsx
@@ -0,0 +1,13 @@
+import "../../App.scss"
+
+function Orders() {
+    return (
+        <div className="Orders">
+            <div className="orders-box">
+
+            </div>
+        </div>
+    )
+}
+
+export default Orders
