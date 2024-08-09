b55d613a3d4f986eadc3def6f7b5fcff2f7852f3
Sahil Kalkal <sahilkalkal108@gmail.com>
2024-08-09 21:54:01 +0530

    story page

--- src/components/story/story.jsx
+++ src/components/story/story.jsx
@@ -14,11 +14,25 @@ function Story() {
                             Our Story
                         </p>
                         <p className="story-content">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                         </p>
                         <button className="view-more">
                             View Work
                         </button>
+                    </div>
+                </div>
+                <div className="story-section rf">
+                    <div className="story-left">
+                        <img className="simg" src={simgg} />
+                    </div>
+                    <div className="story-right">
+                        <p className="head fh">
+                            Who are we ?
+                        </p>
+                        <p className="story-content">
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                        </p>
+                        
                     </div>
                 </div>
             </div>
